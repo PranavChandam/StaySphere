0a31dbf97dee54f7a5f960d8993ab67854683cdb
Pranav Chandam <pranavchandam898@gmail.com>
2025-06-28 21:55:37 +0530

    added navber and footer

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const port= 8080
 const path= require('path')
 const Listing = require('./models/listing.js')
 const methodOverride= require('method-override')
+const ejsMate= require('ejs-mate')
 
 app.set('view engine','ejs')
 app.set("views",path.join(__dirname,"views"))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
+app.use(express.static(path.join(__dirname,"/public")))
+app.engine('ejs',ejsMate)
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)

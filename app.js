667e0082f041e68016490fe6f4a7c1ba899487b2
Pranav Chandam <pranavchandam898@gmail.com>
2025-07-06 23:12:29 +0530

    Create User Schema and implement passport middleware

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const ejsMate= require('ejs-mate')
 const ExpressError= require('./utils/ExpressError.js')
 const session=require("express-session")
 const flash=require("connect-flash")
+const passport=require('passport')
+const LocalStrategy= require("passport-local")
+const User=require('./models/user.js')
 
 const listings=require('./routes/listing.js')
 const reviews=require('./routes/review.js')
@@ -34,11 +37,27 @@ const sessionOptions={
 app.use(session(sessionOptions))
 app.use(flash())
 
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 
 app.use((req,res,next)=>{
     res.locals.success=req.flash('success')
     res.locals.error=req.flash('error')
     next()
+})
+
+app.use("/demoUser",async(req,res)=>{
+    let fakeUser= new User({
+        email:"student@gmail.com",
+        username:"student",
+    })
+
+    let registerUser= await User.register(fakeUser,"HelloWorld")
+    res.send(registerUser)
 })
 
 app.listen(port,()=>{

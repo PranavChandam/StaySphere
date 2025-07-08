ba90b65751a1bca50a231c8a360c4791dffe18d5
Pranav Chandam <pranavchandam898@gmail.com>
2025-07-08 17:42:01 +0530

    Add login method to bypass login after new user signup. 2]save redirect url to redirect useron same path after login

--- middleware.js
+++ middleware.js
@@ -1,7 +1,15 @@
 module.exports.isLoggedIn= (req,res,next)=>{
      if(!req.isAuthenticated()){
+       req.session.redirectUrl=req.originalUrl
        req.flash('error','You must be logged In First')
        return res.redirect('/login')   //return require when you send twice responce
     }
     next()
+}
+
+module.exports.saveRedirectUrl=(req,res,next)=>{
+  if(req.session.redirectUrl){
+    res.locals.redirectUrl=req.session.redirectUrl
+  }
+  next()
 }
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router =express.Router()
 const User=require('../models/user.js')
 const wrapAsync = require('../utils/wrapAsync.js')
 const passport = require('passport')
+const { saveRedirectUrl } = require('../middleware.js')
 
 
 router.get('/signup',(req,res)=>{
@@ -14,8 +15,13 @@ router.post("/signup",wrapAsync( async(req,res)=>{
     let {username,email,password}=req.body
     const newUser= new User({username,email})
     const registerUser= await User.register(newUser,password)
+    req.login(registerUser,(err)=>{    //auto login after signup by using login()  (new user)
+      if(err){
+        return next(err)
+      }
     req.flash('success',"User Register Successfully")
-    res.redirect('/login')
+    res.redirect('/listing')
+    })
   }
   catch(err){
     req.flash('error',err.message)
@@ -28,11 +34,12 @@ router.get('/login',(req,res)=>{
     res.render('users/login.ejs')
 })
 
-router.post("/login",
+router.post("/login",saveRedirectUrl,
     passport.authenticate("local",{failureRedirect: '/login',failureFlash:true}), //middleware for authentication
     async(req,res)=>{
       req.flash("success","Welcome Back To WanderLust")
-      res.redirect('/listing')
+      let redirectUrl=res.locals.redirectUrl || "/listing"
+      res.redirect(redirectUrl)
 })
 
 router.get('/logout',(req,res,next)=>{
